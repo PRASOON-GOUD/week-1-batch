564afb83ab7b0dcde4f81ab0fe30752bd455e4bc
PRASOON-GOUD <prasoongoud04@gmail.com>
2025-01-09 13:20:54 +0530

    New files

--- functions.js
+++ functions.js
@@ -3,9 +3,54 @@
 // }
 // demo();
 
-function add1(a,b){
-    console.log(a+b);
-}
-add1(5,6);
-add1(9,3);
-add1(8,4);
+// function add1(a,b){
+//     console.log(a+b);
+// }
+// add1(5,6);
+// add1(9,3);
+// add1(8,4);
+
+// function isPalindrome(str){
+//     let revStr = "";
+//     for(let i=str.length-1;i>=0;i--){
+//         // console.log(str[i]);
+//         revStr += str[i];
+//     }
+//     if(str == revStr){
+//         console.log("Palindrome");
+//     }else{
+//         console.log("Not Palindrome");
+//     }
+// }
+// isPalindrome("sir")
+// isPalindrome("malayalam")
+// isPalindrome("madam")
+
+// 1) function expression for anonymous
+// let x = function(){
+//     console.log("Function expression");
+// }
+// x();
+
+// // 2)
+// (
+//     function(){
+//         console.log("IIFE");
+//     }
+// )();
+
+
+// // arrow function
+// let y = _=>{console.log("Hello");
+// }
+// y();
+
+// let z = a=>{console.log(a);
+// } 
+// z(10);
+
+// function add(a,b){
+//     return a+b;
+// }
+// add(10,9);
+
